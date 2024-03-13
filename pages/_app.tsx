8fa1614a124ccb9b9d2b25a2d5699098c214e2ae
siskos-k <konstantinos.siscos@gmail.com>
2024-03-13 15:54:33 +0200

    Modals and Button Components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import Layout from "@/components/Layout";
 import "../styles/globals.css";
+import Modal from "@/components/Modal";
 
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
+    {/* <Modal actionLabel="Submit" title="Test Modal"isOpen} /> */}
   <Layout>
   <Component {...pageProps} />
   </Layout>
+  </>
   )
 }
