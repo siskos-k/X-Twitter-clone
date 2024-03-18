15e318aa496a3912d04e0ed9df37b277401e3c85
siskos-k <konstantinos.siscos@gmail.com>
2024-03-18 16:43:47 +0200

    added Input modal

---
+++ components/Input.tsx
@@ -0,0 +1,46 @@
+interface InputProps {
+    placeHolder?: string;
+    value?: string;
+    type?: string;
+    disabled?: boolean;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+
+}
+
+
+const Input: React.FC<InputProps> = (
+    {
+        placeHolder,
+        value,
+        type,
+        disabled,
+        onChange
+    }
+) =>{
+ return(
+ <input 
+ placeholder={placeHolder}
+ disabled={disabled}
+ value={value}
+ type={type}
+ onChange={onChange}
+ className="
+ w-full
+ p-4
+ text-lg
+ bg-black
+ border-2
+ border-neutral-800
+ rounded-md
+ outline-none
+ text-white
+ focus:border-sky-500
+ focus:border-2
+ transition
+ disabled:bg-neutral-900
+ disabled:opacity-70
+ disabled:cursor-not-allowed
+ "
+ /> );  
+}
+export default Input
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import Layout from "@/components/Layout";
 import "../styles/globals.css";
 import Modal from "@/components/Modal";
+import LoginModal from "@/components/modals/LoginModal";
 
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    {/* <Modal actionLabel="Submit" title="Test Modal"isOpen} /> */}
+  {/* <Modal actionLabel="Submit" title="Test Modal"isOpen /> */}
+  <LoginModal/>
   <Layout>
   <Component {...pageProps} />
   </Layout>
