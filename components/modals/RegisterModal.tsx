093e936ec5ae8d9a9d3219b84bf9c1877ee4a58e
siskos-k <konstantinos.siscos@gmail.com>
2024-03-20 00:10:46 +0200

    Login-Register Modals and toggle

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -1,36 +1,39 @@
 import useLoginModal from "@/hooks/useLoginModal";
+import useRegisterModal from "@/hooks/useRegisterModal";
+
 import { useCallback, useState } from "react";
 import Input from "../Input";
 import Modal from "../Modal";
-import RegisterModal from "./RegisterModal";
-import useRegisterModal from "@/hooks/useRegisterModal";
 
-const LoginModal = () => {
+const RegisterModal = () => {
     const loginModal = useLoginModal();
     const registerModal = useRegisterModal();
 
+    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
     const [isLoading, setIsLoading]= useState(false);
+
     const onSubmit = useCallback(async()=>{
         try{
         setIsLoading(true);
-        loginModal.onClose();
+        registerModal.onClose();
         } catch(error){
         console.log(error);
         }
         finally{
             setIsLoading(false);
         }
-        },[loginModal]);
+        },[registerModal]);
 
-        const onToggle = useCallback (()=>{
-            if(isLoading)return;
+    const onToggle = useCallback(()=>{
+        // if(isLoading)return;
 
-            loginModal.onClose();
-            registerModal.onOpen();
-            
-        },[isLoading, registerModal, loginModal]);
+        loginModal.onOpen();
+        registerModal.onClose();
+    },[isLoading, registerModal, loginModal])
         
         const bodyContent = (
             <div className="flex flex-col gap-4" >
@@ -41,6 +44,18 @@ const LoginModal = () => {
                 disabled={isLoading}
                 />
                 <Input
+                placeHolder="Name"
+                onChange={(e)=>setName(e.target.value)}
+                value={name}
+                disabled={isLoading}
+                />
+                 <Input
+                placeHolder="Username"
+                onChange={(e)=>setUsername(e.target.value)}
+                value={username}
+                disabled={isLoading}
+                />
+                 <Input
                 placeHolder="Password"
                 onChange={(e)=>setPassword(e.target.value)}
                 value={password}
@@ -50,20 +65,20 @@ const LoginModal = () => {
         )
         const footerContent = (
             <div className="text-neutral-400 text-center mt-4">
-                <p>Don't have an account?</p>
-                <span onClick={onToggle} className="text-white cursor-pointer hover:underline">  Sign up  </span>
+                <p>Already have an account?</p>
+                <span onClick={onToggle} className="text-white cursor-pointer hover:underline">  Sign in  </span>
 
             </div>
         )
     return(<Modal
     disabled={isLoading}
-    isOpen={loginModal.isOpen}
-    title="Log In"
-    actionLabel="Sign In"
-    onClose={loginModal.onClose}
+    isOpen={registerModal.isOpen}
+    title="Create an Account"
+    actionLabel="Register"
+    onClose={registerModal.onClose}
     onSubmit={onSubmit}
     body={bodyContent}
     footer={footerContent}
     />);
 }
-export default LoginModal;
+export default RegisterModal;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,14 @@ import Layout from "@/components/Layout";
 import "../styles/globals.css";
 import Modal from "@/components/Modal";
 import LoginModal from "@/components/modals/LoginModal";
+import RegisterModal from "@/components/modals/RegisterModal";
 
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-  {/* <Modal actionLabel="Submit" title="Test Modal"isOpen /> */}
+  <RegisterModal/>
   <LoginModal/>
   <Layout>
   <Component {...pageProps} />
